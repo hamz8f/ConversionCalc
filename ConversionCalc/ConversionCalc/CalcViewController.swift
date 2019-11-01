239aadbf9f21d317ce198b654ecc72b4fb92bbfe
Henry McMurray <hamz8f@mail.missouri.edu>
2019-11-01 10:10:18 -0500

    init

---
+++ ConversionCalc/ConversionCalc/CalcViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CalcViewController.swift
+//  ConversionCalc
+//
+//  Created by Henry McMurray on 11/1/19.
+//  Copyright © 2019 Henry McMurray. All rights reserved.
+//
+
+import SwiftUI
+
+struct CalcViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalcViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        CalcViewController()
+    }
+}
---
+++ ConversionCalc/ConversionCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConversionCalc
+//
+//  Created by Henry McMurray on 11/1/19.
+//  Copyright © 2019 Henry McMurray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
